da3d5aa849081a0af6fd5c0be9f7cab4da9fece2
karmakarila5-star <karmakarila5@gmail.com>
2026-06-06 18:31:33 +0530

    homework type

---
+++ homeworktype.py
@@ -0,0 +1,8 @@
+# Ram inputs his congratulation message in lowercase
+message = input("Ram, enter your congratulatory message: ")
+
+# Convert the string to uppercase
+uppercased_message = message.upper()
+
+# Display the final message
+print("Converted Message:", uppercased_message)
