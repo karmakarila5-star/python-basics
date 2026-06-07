fcd5fb2fe009b475979a8c51d188c9c2ffcced2d
karmakarila5-star <karmakarila5@gmail.com>
2026-06-07 13:48:58 +0530

    operator

---
+++ percentage.py
@@ -0,0 +1,8 @@
+print("Enter Marks Obtainted in 4 Subjects: ")
+math = int(input("Maths :"))
+english = int(input("english :"))
+science = int(input("science :"))
+hindi = int(input("hindi :"))
+sum = math +english+science+hindi
+perc = (sum/400)*100
+print ("percentage Mark = ",perc)
